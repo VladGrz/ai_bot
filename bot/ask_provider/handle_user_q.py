37eb3866a223d29bb5222c54cfc60a3c086d3235
VladGrz <vladgrozny2@gmail.com>
2024-01-22 00:16:48 +0200

    Added command to ask all provider at once

--- bot/ask_provider/handle_user_q.py
+++ bot/ask_provider/handle_user_q.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiogram.types import Message, CallbackQuery
 from loader import dp, bot
 
@@ -10,25 +12,34 @@ async def answer_bing(message: Message):
 
 
 @dp.message_handler(commands=["gptgo"])
-async def answer_bing(message: Message):
+async def answer_gptgo(message: Message):
     await message.answer(await ask_provider(_providers["GptGo"], message.text[7:]))
 
 
 @dp.message_handler(commands=["you"])
-async def answer_bing(message: Message):
+async def answer_you(message: Message):
     await message.answer(await ask_provider(_providers["You"], message.text[5:]))
 
 
 @dp.message_handler(commands=["chatbase"])
-async def answer_bing(message: Message):
+async def answer_chatbase(message: Message):
     await message.answer(await ask_provider(_providers["ChatBase"], message.text[10:]))
 
 
 @dp.message_handler(commands=["phind"])
-async def answer_bing(message: Message):
+async def answer_phind(message: Message):
     await message.answer(await ask_provider(_providers["Phind"], message.text[7:]))
 
 
 @dp.message_handler(commands=["liaobots"])
-async def answer_bing(message: Message):
+async def answer_liaobots(message: Message):
     await message.answer(await ask_provider(_providers["Liaobots"], message.text[10:]))
+
+
+@dp.message_handler(commands=["all"])
+async def answer_all(message: Message):
+    results = [
+        await message.answer(
+            await ask_provider(provider_id, message.text[5:])
+        ) for provider_id in _providers.values()
+    ]
